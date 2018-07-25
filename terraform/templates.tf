cf4e2310c2193b773402432d8dee296fed46cb1d
grayi <iain.gray@elsevier.com>
2018-07-25 13:31:07 +0100

    fixing nomad worker node cluster join issues

--- terraform/templates.tf
+++ terraform/templates.tf
@@ -31,4 +31,8 @@ data "template_file" "container_server_user_data" {
 #--------------------------------------------------------------
 data "template_file" "container_worker_user_data" {
   template = "${file("provisioning/templates/container-worker-cloud-config.tpl")}"
+  vars {
+    cluster_tag_key   = "${var.container_server_consul_tag_key}"
+    cluster_tag_value = "${var.container_server_consul_tag_value}"
+  }
 }
